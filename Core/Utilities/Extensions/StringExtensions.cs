23d15a5707a43e50539b451fe889a704c2f2a1dc
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-06-05 21:30:23 +0300

    Refactor ui

---
+++ Core/Utilities/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Utilities.Extensions;
+
+public static class StringExtensions
+{
+    public static string ShortText(this string text, int length = 150)
+    {
+        return text.Substring(0, Math.Min(text.Length, length)) + "...";
+    }
+}
