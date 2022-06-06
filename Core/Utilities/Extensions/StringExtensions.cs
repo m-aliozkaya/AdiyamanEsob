f7e6aae7f169294fe4793c73934e157fa3499ddb
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-06-06 19:31:16 +0300

    Update admin layout
    Sidebar updated

--- Core/Utilities/Extensions/StringExtensions.cs
+++ Core/Utilities/Extensions/StringExtensions.cs
@@ -1,9 +1,16 @@
-﻿namespace Core.Utilities.Extensions;
+﻿using System.Text.RegularExpressions;
+
+namespace Core.Utilities.Extensions;
 
 public static class StringExtensions
 {
     public static string ShortText(this string text, int length = 150)
     {
         return text.Substring(0, Math.Min(text.Length, length)) + "...";
+    }
+    
+    public static string StripTags(this string text)
+    {
+        return Regex.Replace(text, "<.*?>|&.*?;", string.Empty);
     }
 }
