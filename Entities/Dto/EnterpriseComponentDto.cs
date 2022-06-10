4d80c8eba64cab48d53f6c094910ca4f390c43a2
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-06-10 20:54:17 +0300

    Add dynamic home page

---
+++ Entities/Dto/EnterpriseComponentDto.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Entity;
+
+namespace Entities.Dto;
+
+public class EnterpriseComponentDto
+{
+    public List<Organization> Organization { get; set; }
+    public List<AboutArticle> AboutArticles { get; set; }
+}
