ae05f992c2b27bab5e163b538cdd292efab27bc1
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-06-12 13:21:31 +0300

    Add announcements to home page

---
+++ UI/Pages/Announcement/AnnouncementSingle.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages.Announcement;
+
+public class AnnouncementSingle : PageModel
+{
+    private readonly IAnnouncementService _announcementService;
+    public Entities.Entity.Announcement Announcement { get; set; }
+
+    public AnnouncementSingle(IAnnouncementService announcementService)
+    {
+        _announcementService = announcementService;
+    }
+    
+    public async Task<IActionResult> OnGet(string seoUrl)
+    {
+        var result = await _announcementService.GetBySeoUrlAsync(seoUrl);
+
+        if (!result.Success)
+        {
+            return RedirectToPage("./NotFound");
+        }
+
+        Announcement = result.Data;
+        return Page();
+    }
+}
---
+++ UI/Pages/Announcement/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages.Announcement;
+
+public class Index : PageModel
+{
+    private readonly IAnnouncementService _announcementService;
+    private const int RecordsPerPage = 8;
+    public PagingDto Paging { get; set; }
+    public List<Entities.Entity.Announcement> Announcements { get; set; }
+
+    public Index(IAnnouncementService announcementService)
+    {
+        _announcementService = announcementService;
+    }
+    
+    public async Task<IActionResult> OnGet(int currentPage)
+    {
+        var result = await _announcementService.GetAllByPage(RecordsPerPage, currentPage);
+        Announcements = result.Data.Announcements;
+
+        Paging = new PagingDto
+        {
+            TotalRecords = result.Data.TotalRecors,
+            CurrentPage = currentPage,
+            RecordsPerPage = RecordsPerPage
+        };
+
+        return Page();
+    }
+}
